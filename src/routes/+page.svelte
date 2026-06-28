cf16b260babfb598ee605c9e310d1721a28fe1de
dhruvmane <dhruvmaneofficial@gmail.com>
2026-06-29 02:16:59 +0530

    camcorder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,19 @@
-<script>
-    import DEADISM from '$lib/components/projects/Deadism.svelte'
+<script lang=ts>
+    import Image from '$lib/assets/images/camcorder.png'
+    import Wordmark from '$lib/assets/images/camcorder-wordmark-invert.png'
+    import { onMount } from 'svelte';
+    import gsap from "gsap";
+
+    let icon: HTMLElement;
+
+
 </script>
 
-<!-- Projects -->
-<div class="absolute top-0 left-1/2 -translate-x-1/2 justify-center min-w-150 min-h-full bg-white dark:bg-black">
-    
-    <title>flowergardenstudios</title>
-    
-    <!-- DEADISM -->
-    <DEADISM />
-</div>
-
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Imbue:opsz,wght@10..100,100..900&display=swap');
-    @import url('https://fonts.googleapis.com/css2?family=Jersey+25&display=swap');
-
-</style>
+<main class="">
+    <div class="absolute h-screen w-screen">
+        <img class="h-screen w-screen brightness-30" alt="a" src={Image}>
+        <div bind:this={icon} class="absolute top-[80%] -translate-y-1/2 left-[80%] -translate-x-1/2">
+             <img class="invert w-100" alt="a" src={Wordmark}>
+        </div>
+    </div>
+</main>
